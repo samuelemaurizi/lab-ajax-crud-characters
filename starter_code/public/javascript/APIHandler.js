92469169c230ab02c2c01144aa2848b335f567ba
Samuele Maurizi <samuelemaurizi@gmail.com>
2018-10-28 15:07:35 +0100

    Done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,87 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    // Create Axios new instance
+    this.query = axios.create({
+      baseURL: this.BASE_URL
+    });
   }
 
-  getFullList () {
-
+  // FULL LIST
+  getFullList() {
+    return this.query
+      .get("/characters")
+      .then(response => {
+        console.log(`GET LIST OF ${response.data.length} CHARACTERS!`);
+        return response.data;
+      })
+      .catch(err => {
+        throw err;
+      });
   }
 
-  getOneRegister () {
-
+  // GET ONE
+  getOneRegister(characterId) {
+    return this.query
+      .get("/characters/" + characterId)
+      .then(response => {
+        console.log(response.data);
+        $("input[name=character-id]").val("");
+        $("#fetch-one").css("background-color", "green");
+        return response.data;
+      })
+      .catch(err => {
+        $("#fetch-one").css("background-color", "red");
+        throw err;
+      });
   }
 
-  createOneRegister () {
-
+  // POST ONE
+  createOneRegister(newChar) {
+    return this.query
+      .post("/characters/", newChar)
+      .then(response => {
+        console.log(response.data);
+        $("#new-character-form :input").val("");
+        $("#send-data").css("background-color", "green");
+      })
+      .catch(err => {
+        $("#send-data").css("background-color", "red");
+        throw err;
+      });
   }
 
-  updateOneRegister () {
-
+  // PATCH ONE
+  updateOneRegister(charId, updatedChar) {
+    this.query
+      .patch(`/characters/${charId}`, updatedChar)
+      .then(response => {
+        console.log("CHARACTER UPDATE", response.data);
+        $("#edit-character-form :input").val();
+        $("#update-data").css("background-color", "green");
+      })
+      .catch(err => {
+        $("#update-data").css("background-color", "red");
+        console.log(err.response);
+      });
   }
 
-  deleteOneRegister () {
-
+  // DELTE ONE BY ID
+  deleteOneRegister(charId) {
+    return this.query
+      .delete("/characters/" + charId)
+      .then(response => {
+        console.log("CHARACTER DELETED! ID: ", charId);
+        $("input[name=character-id-delete]").val("");
+        $("#delete-one").css("background-color", "green");
+        return response.data;
+      })
+      .catch(err => {
+        $("#delete-one").css("background-color", "red");
+        throw err;
+      });
   }
 }
+
+// Run the following code on the terminal to make our API start working
+// $ json-server --watch api/db.json --port 8000
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,145 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  function reset() {
+    $(".characters-container").html("");
+  }
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  // GET ALL
+  document.getElementById("fetch-all").onclick = function() {
+    reset();
+    charactersAPI
+      .getFullList("/characters")
+      .then(characters => {
+        for (let i = 0; i < characters.length; i++) {
+          $(".characters-container").append(
+            `
+            <div class="character-info">
+            <div class="char-id"><strong>Id: </strong> ${
+              characters[i].id
+            } </div>
+            <div class="name"><strong>Name: </strong> ${
+              characters[i].name
+            } </div>
+            <div class="occupation"><strong>Occupation: </strong> ${
+              characters[i].occupation
+            } </div>
+            <div class="cartoon"><strong>Cartoon: </strong> ${
+              characters[i].cartoon ? "yes" : "no"
+            } </div>
+            <div class="weapon"><strong>Weapon: </strong> ${
+              characters[i].weapon
+            } </div>
+            </div>
+            `
+          );
+        }
+      })
+      .catch(err => {
+        throw err;
+      });
+  };
+
+  // GET ONE
+  document.getElementById("fetch-one").onclick = function() {
+    reset();
+    let charId = $("input[name=character-id]").val();
+    charactersAPI
+      .getOneRegister(charId)
+      .then(character => {
+        $(".characters-container").append(
+          `
+          <div class="character-info">
+            <div class="char-id"><strong>Id: </strong> ${character.id} </div>
+            <div class="name"><strong>Name: </strong> ${character.name} </div>
+            <div class="occupation"><strong>Occupation: </strong> ${
+              character.occupation
+            } </div>
+            <div class="cartoon"><strong>Cartoon: </strong> ${
+              character.cartoon
+            } </div>
+            <div class="weapon"><strong>Weapon: </strong> ${
+              character.weapon
+            } </div>
+            </div>
+          `
+        );
+      })
+      .catch(err => {
+        throw err;
+      });
+  };
+
+  // DELETE
+  document.getElementById("delete-one").onclick = function() {
+    reset();
+    let charId = $("input[name=character-id-delete]").val();
+    charactersAPI
+      .deleteOneRegister(charId)
+      .then(character => {
+        $(".characters-container").append(
+          `<div class="char-id">Character with Id ${charId} has been successfully deleted</div>`
+        );
+      })
+      .catch(err => {
+        $(".characters-container").append(
+          `<div class="char-id">Character with Id ${charId} not found</div>`
+        );
+        throw err;
+      });
+  };
+
+  // POST
+  document.getElementById("new-character-form").onsubmit = function(e) {
+    e.preventDefault();
+    reset();
+
+    let createChar = {
+      name: $("input[name=name]").val(),
+      occupation: $("input[name=occupation]").val(),
+      weapon: $("input[name=weapon]").val(),
+      cartoon: $("input[name=cartoon]").prop("checked")
+    };
+
+    charactersAPI
+      .createOneRegister(createChar)
+      .then(character => {
+        $(".characters-container").append(
+          `
+        <div class="character-info">
+          <div class="name"><strong>Name: </strong> ${createChar.name} </div>
+          <div class="occupation"><strong>Occupation: </strong> ${
+            createChar.occupation
+          } </div>
+          <div class="weapon"><strong>Weapon: </strong> ${
+            createChar.weapon
+          } </div>
+          <div class="cartoon"><strong>Cartoon: </strong> ${
+            createChar.cartoon ? "Yes" : "No"
+          } </div>
+          </div>
+        `
+        );
+      })
+      .catch(err => {
+        throw err;
+      });
+  };
+
+  // PATCH
+  document.getElementById("edit-character-form").onsubmit = function(e) {
+    e.preventDefault();
+    reset();
+
+    let updatedChar = {
+      name: $("#edit-character-form :input[name=name]").val(),
+      occupation: $("#edit-character-form :input[name=occupation]").val(),
+      weapon: $("#edit-character-form :input[name=weapon]").val(),
+      cartoon: $("#edit-character-form :input[name=cartoon]").prop("checked")
+    };
+
+    let charId = $("#edit-character-form :input[name=chr-id]").val();
+
+    charactersAPI.updateOneRegister(charId, updatedChar);
+  };
+});
